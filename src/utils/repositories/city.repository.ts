c05195325c4224f1f927b4536db2ff71340f7fa3
christelle marquier <christellemonv@gmail.com>
2023-03-27 10:48:19 +0200

    branch

---
+++ src/utils/repositories/city.repository.ts
@@ -0,0 +1,24 @@
+import City from "../../entities/City.entity";
+import databaseConfig from "../../config/typeorm";
+
+export const CityRepository = databaseConfig.getRepository(City)
+    .extend({
+        async findByLatitudeAndByLongitude(latitude: string, longitude: string): Promise<City | null> {
+            return this.createQueryBuilder("city")
+                .where("latitude = :latitude", {latitude})
+                .andWhere("longitude = :longitude", {longitude})
+                .getOne()
+        },
+        async findByNameAndIfNotID(id: number, name: string): Promise<City | null> {
+            return this.createQueryBuilder("city")
+                .where("name = :name", {name})
+                .andWhere("id != :id", {id})
+                .getOne()
+        },
+        async findByLatitudeAndByLongitudeIfNotID(id: number, latitude: string, longitude: string): Promise<City | null> {
+            return this.createQueryBuilder("city")
+                .where("latitude = :latitude AND longitude = :longitude", {latitude, longitude})
+                .andWhere("id != :id", {id})
+                .getOne()
+        }
+    });
---
+++ src/utils/repositories/role.repository.ts
@@ -0,0 +1,4 @@
+import Role from "../../entities/Role.entity";
+import databaseConfig from "../../config/typeorm";
+
+export const RoleRepository = databaseConfig.getRepository(Role).extend({});
