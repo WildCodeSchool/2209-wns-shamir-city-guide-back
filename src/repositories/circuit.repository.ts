18ce4e5beb9aeace42ee1e06a7ec44dc7bcb1214
shishifoxy <shirley.bsth@gmail.com>
2023-03-31 11:20:25 +0200

    "ajout possibilité de récuperer les circuits à partir d'une ville"

--- src/repositories/circuit.repository.ts
+++ src/repositories/circuit.repository.ts
@@ -1,4 +1,10 @@
 import Circuit from "../entities/Circuit.entity";
 import databaseConfig from "../config/typeorm";
 
-export const CircuitRepository = databaseConfig.getRepository(Circuit).extend({});
+export const CircuitRepository = databaseConfig.getRepository(Circuit).extend({
+    async findAllCircuitsByVille(villeId :number) : Promise <Circuit | null> {
+        return this.createQueryBuilder("circuit")
+            .where("city=: city", {villeId})
+    }
+
+});
